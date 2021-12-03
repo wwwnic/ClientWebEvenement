58c98e0f5c281e7afbe7d14fcc8555c66ddb147a
Nicolas Brunet <1816098@crosemont.qc.ca>
2021-12-03 17:38:41 -0500

    ajout d'une validation béton dans le modèle utilisateur

--- ApplicationWebEvenements/ApplicationWebEvenements/Models/ValidationUtilisateur.cs
+++ ApplicationWebEvenements/ApplicationWebEvenements/Models/ValidationUtilisateur.cs
@@ -12,30 +12,35 @@ namespace ApplicationWebEvenements.Models
 
         [Required(ErrorMessage = "Nom utilisateur requis")]
         [Display(Name = "Nom utilisateur")]
-        [StringLength(20, MinimumLength = 4)]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Le nom utilisateur doit être inclusivement entre 4 et 20 caractères")]
         [DataType(DataType.Text, ErrorMessage = "Nom utilisateur invalide")]
+        [RegularExpression(@"^(?=[a-zA-Z0-9._]{0,21}$)(?!.*[_.]{2})[^_.].*[^_.]$", ErrorMessage = "Nom d'utilisateur interdit")]
+
         public string NomUtilisateur { get; set; }
 
 
         [Required(ErrorMessage = "Mot de passe requis")]
         [Display(Name = "Mot de passe")]
-        [StringLength(24, MinimumLength = 4)]
-        [DataType(DataType.Password, ErrorMessage = "Mot de passe invalide")]
+        [StringLength(24, MinimumLength = 4, ErrorMessage = "Le mot de passe doit être inclusivement entre 4 et 24 caractères")]
+        [DataType(DataType.Password, ErrorMessage = "Mot de passe non autorisé")]
+        [RegularExpression(@"[a-zA-Z0-9\\!\\@\\#\\$\\*\\&\(\\$\\?\\%\\^\\'\\|\\)]{0,25}", ErrorMessage = "Mot de passe non autorisé : A-Z, 0-9 et certain symbole uniquement")]
+
         public string MotDePasse { get; set; }
 
 
         [Required(ErrorMessage = "Courriel requis")]
         [Display(Name = "Courriel")]
-        [StringLength(30)]
-        [DataType(DataType.EmailAddress, ErrorMessage = "Courriel invalide")]
+        [StringLength(30, ErrorMessage = "Le courriel ne peut pas dépasser 30 caractères")]
+        [EmailAddress(ErrorMessage = "Adresse courriel invalide")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Adresse courriel invalide")]
+
         public string Courriel { get; set; }
 
         [Required(ErrorMessage = "Téléphone requis")]
         [Display(Name = "Téléphone")]
-        [StringLength(12)]
+        [StringLength(12, ErrorMessage = "Le téléphone ne peut pas dépasser 12 chiffres")]
         [DataType(DataType.PhoneNumber)]
         [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Numéro de téléphone invalide")]
-
         public string Telephone { get; set; }
     }
 }
