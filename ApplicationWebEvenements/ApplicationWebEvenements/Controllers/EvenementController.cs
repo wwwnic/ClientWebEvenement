d6ba8c08a772e7804794b0c276a2e1913967fd81
SMartinCyr <smartincyr@dept-info.crosemont.quebec>
2021-12-05 13:48:40 -0500

    vérification après merge

--- ApplicationWebEvenements/ApplicationWebEvenements/Controllers/EvenementController.cs
+++ ApplicationWebEvenements/ApplicationWebEvenements/Controllers/EvenementController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using ApplicationWebEvenements.Hubs;
 
 namespace ApplicationWebEvenements.Controllers
